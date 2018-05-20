55d57653a929bf2a3507e3a86665949e30d6565f
crocodile3 <yrt333333@163.com>
2018-05-20 22:57:46 +0800

    使用多线程操作数据库

--- MysqlOperate.py
+++ MysqlOperate.py
@@ -1,5 +1,9 @@
 # -*- coding:utf-8 -*-
+from threading import Thread
+
 import pymysql
+import time
+
 
 class Mysql():
     def __init__(self,host,user,passwd,db,port=3306):
@@ -37,3 +41,49 @@ class Mysql():
         self.cursor.close()
         self.conn.close()
 
+
+
+result = []
+class MyThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        global result
+        conn = Mysql('localhost','root','mysql','goods')
+        result.append(conn.scaler("select sql_no_cache count(`id`) from `data_" + str(self.no) +"` where `title` like '%hello%'"))
+
+def task():
+    thr1 = MyThread()
+    thr2 = MyThread()
+    thr3 = MyThread()
+    thr4 = MyThread()
+
+    thr1.start()
+    thr2.start()
+    thr3.start()
+    thr4.start()
+
+    thr1.join()
+    thr2.join()
+    thr3.join()
+    thr4.join()
+
+    return True
+
+if __name__ == "__main__":
+
+    print ""
+    print "...... multi thread query start ......"
+    print time.ctime() + ' / ' + str(time.time())
+    task()
+    print result
+    print time.ctime() + ' / ' + str(time.time())
+    print "...... multi thread query end ......"
+
+    print ""
+    dbCon = Mysql('localhost', 'root', '123456', 'mydb')
+    print "...... single thread query start ......"
+    print time.ctime() + ' / ' + str(time.time())
+    print dbCon.scaler("select sql_no_cache count(`id`) from `data` where `title` like '%hello%'")
+    print time.ctime() + ' / ' + str(time.time())
