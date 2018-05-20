b5caa372f716189beaf7390f99f7483a1911ba9e
crocodile3 <yrt333333@163.com>
2018-05-20 14:15:54 +0800

    呆完善

---
+++ MysqlOperate.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+import pymysql
+
+class Mysql():
+    def __init__(self,host,user,passwd,db,port=3306):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.db = db
+        self.port = port
+        self.connect()
+
+    def connect(self):
+        self.conn = pymysql.connect(host = self.host, user = self.user, passwd = self.passwd, db = self.db, port = self.port)
+        self.cursor = self.conn.cursor()
+
+    def execute(self,sql):
+        result = self.cursor.execute(sql)
+        return result
+
+    def query(self,sql):
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        return result
+
+    def scaler(self,sql):
+        self.cursor.execute(sql)
+        result = self.cursor.fetchone()
+        return result[0]
+
